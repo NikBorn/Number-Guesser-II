c1fafc2f1c3ca0a84c6da7f807f72336b6db7321
Nik Bornemeier <NikolausRB@gmail.com>
2017-03-17 12:14:56 -0600

    complete HTML, start CSS, create random number in JS

---
+++ scripts.js
@@ -0,0 +1,9 @@
+
+var rightNumber = correctNumber();
+var guessedNumber = document.querySelector()
+
+function correctNumber() {
+  var randomNum = Math.floor(Math.random() * 200 +1);
+  var newNumber=parseInt(randomNum);
+  return newNumber;
+}
