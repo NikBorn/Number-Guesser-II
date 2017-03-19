9ec38d34d87a031f6ea371436fa6c4bf708aaae2
Nik Bornemeier <NikolausRB@gmail.com>
2017-03-19 17:19:45 -0600

    complete phase 2 of JS

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 
 var rightNumber = correctNumber();
 var guessButton = document.querySelector(".guess-btn");
+var clearButton = document.querySelector(".clear-btn");
+var resetButton = document.querySelector(".reset-btn")
 
 function correctNumber() {
   var randomNum = Math.floor(Math.random() * 100 +1);
@@ -8,18 +10,15 @@ function correctNumber() {
   return newNumber;
 }
 correctNumber();
-
-guessButton.addEventListener("click", function(){
-  event.preventDefault();
-  if ()
-  compareNumbers();
-}
-)
+console.log(rightNumber);
 
 function compareNumbers() {
   var guessedNumber = document.querySelector(".user-guess").value;
   document.querySelector(".display-guess").innerText = guessedNumber
   document.querySelector("#top-text").innerText = "Your Last Guess Was";
+  if (guessedNumber > 100 || guessedNumber < 1) {
+    alert("Your Guess was Outside of Range");
+  }
   if (guessedNumber > rightNumber) {
     document.querySelector("#bottom-text").innerText = "That was Too High.";
   }
@@ -30,7 +29,37 @@ function compareNumbers() {
   }
 };
 
+guessButton.addEventListener("click", function(event){
+  event.preventDefault();
+  compareNumbers();
+}
+)
 
+clearButton.addEventListener("click", function(){
+  document.querySelector(".user-guess").value = "";
+  disableButtons();
+})
 
-console.log(rightNumber);
-// console.log(guessedNumber);
+resetButton.addEventListener("click", function (){
+  correctNumber();
+  document.querySelector(".user-guess").value = "";
+  document.querySelector("#bottom-text").innerText = "";
+  document.querySelector("#top-text").innerText = "";
+  document.querySelector(".display-guess").innerText = "";
+  disableButtons();
+  console.log(correctNumber());
+})
+
+window.addEventListener("keyup", function(){
+  var guessedNumber = document.querySelector(".user-guess").value;
+  console.log(guessedNumber)
+  if (guessedNumber != "") {
+  clearButton.disabled=false;
+  resetButton.disabled=false;
+  }
+})
+
+function disableButtons() {
+  clearButton.disabled=true;
+  resetButton.disabled=true;
+}
