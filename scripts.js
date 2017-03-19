05e99051fe776792336d159129fff073face1a66
Nik Bornemeier <NikolausRB@gmail.com>
2017-03-19 16:18:43 -0600

    complete phase 1 of JS

--- scripts.js
+++ scripts.js
@@ -1,9 +1,36 @@
 
 var rightNumber = correctNumber();
-var guessedNumber = document.querySelector()
+var guessButton = document.querySelector(".guess-btn");
 
 function correctNumber() {
-  var randomNum = Math.floor(Math.random() * 200 +1);
+  var randomNum = Math.floor(Math.random() * 100 +1);
   var newNumber=parseInt(randomNum);
   return newNumber;
 }
+correctNumber();
+
+guessButton.addEventListener("click", function(){
+  event.preventDefault();
+  if ()
+  compareNumbers();
+}
+)
+
+function compareNumbers() {
+  var guessedNumber = document.querySelector(".user-guess").value;
+  document.querySelector(".display-guess").innerText = guessedNumber
+  document.querySelector("#top-text").innerText = "Your Last Guess Was";
+  if (guessedNumber > rightNumber) {
+    document.querySelector("#bottom-text").innerText = "That was Too High.";
+  }
+  if (guessedNumber < rightNumber) {
+    document.querySelector("#bottom-text").innerText = "That was Too Low.";
+  } else if (guessedNumber == rightNumber) {
+    document.querySelector("#bottom-text").innerText = "Boom! Correct Guess!!";
+  }
+};
+
+
+
+console.log(rightNumber);
+// console.log(guessedNumber);
